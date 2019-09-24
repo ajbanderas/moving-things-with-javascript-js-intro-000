e82d86b0c8c864e8c009a5e89924e42af3eaa29f
searching-form-5732 <kevin.downs04@gmail.com>
2019-09-24 12:51:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ function moveDodgerLeft(){
   }
 }
 
+/*function moveDodgerRight(){
+  var leftNumbers = dodger.style.left.replace('px', '');
+  var left = parseInt(leftNumbers, 10);
+  
+  if(left < 360){
+    dodger.style.left = `${left + 1}px`;
+  }
+}*/
+
 function moveDodgerRight(){
   var leftNumbers = dodger.style.left.replace('px', '');
   var left = parseInt(leftNumbers, 10);
@@ -19,8 +28,6 @@ function moveDodgerRight(){
     dodger.style.left = `${left + 1}px`;
   }
 }
-
-
 
 document.addEventListener('keydown', function(e){
   if(e.which == 37){
